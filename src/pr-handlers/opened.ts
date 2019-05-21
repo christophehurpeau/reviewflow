a70a549f94e26e7599b74259431e64072755041c
Christophe Hurpeau <christophe@hurpeau.com>
2019-05-21 19:18:04 +0200

    feat: auto approve when from renovate

--- src/pr-handlers/opened.ts
+++ src/pr-handlers/opened.ts
@@ -1,4 +1,5 @@
 import { Application } from 'probot';
+import { LabelResponse } from '../context/initRepoLabels';
 import { createHandlerPullRequestChange } from './utils';
 import { autoAssignPRToCreator } from './actions/autoAssignPRToCreator';
 import { editOpenedPR } from './actions/editOpenedPR';
@@ -9,10 +10,27 @@ export default function opened(app: Application): void {
     'pull_request.opened',
     createHandlerPullRequestChange(
       async (context, repoContext): Promise<void> => {
+        const fromRenovate = context.payload.pull_request.head.ref.startsWith(
+          'renovate/',
+        );
+
+        if (fromRenovate) {
+          const autoMergeLabel = repoContext.labels['merge/automerge'];
+          const prLabels = context.payload.pull_request.labels;
+          if (
+            autoMergeLabel &&
+            prLabels.find((l: LabelResponse) => l.id === autoMergeLabel.id)
+          ) {
+            await context.github.pulls.createReview(
+              context.issue({ event: 'APPROVE' }),
+            );
+          }
+        }
+
         await Promise.all([
           autoAssignPRToCreator(context, repoContext),
           editOpenedPR(context, repoContext),
-          context.payload.pull_request.head.ref.startsWith('renovate/')
+          fromRenovate
             ? Promise.resolve(undefined)
             : updateReviewStatus(context, repoContext, 'dev', {
                 add: ['needsReview'],
