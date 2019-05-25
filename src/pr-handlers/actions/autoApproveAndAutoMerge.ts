5bc57e0760db4cd41617e1bb054be7436fa1cdbe
Christophe Hurpeau <christophe@hurpeau.com>
2019-05-25 12:37:27 +0200

    fix: renovate adds labels after opening pr

---
+++ src/pr-handlers/actions/autoApproveAndAutoMerge.ts
@@ -0,0 +1,20 @@
+import Webhooks from '@octokit/webhooks';
+import { Context } from 'probot';
+import { RepoContext } from '../../context/repoContext';
+import { autoMergeIfPossible } from './autoMergeIfPossible';
+
+export const autoApproveAndAutoMerge = async (
+  context: Context<Webhooks.WebhookPayloadPullRequest>,
+  repoContext: RepoContext,
+): Promise<void> => {
+  // const autoMergeLabel = repoContext.labels['merge/automerge'];
+  const codeApprovedLabel = repoContext.labels['code/approved'];
+  const prLabels = context.payload.pull_request.labels;
+  if (prLabels.find((l): boolean => l.id === codeApprovedLabel.id)) {
+    await context.github.pulls.createReview(
+      context.issue({ event: 'APPROVE' }),
+    );
+  }
+
+  await autoMergeIfPossible(context, repoContext);
+};
--- src/pr-handlers/opened.ts
+++ src/pr-handlers/opened.ts
@@ -1,27 +1,9 @@
-import Webhooks from '@octokit/webhooks';
-import { Application, Context } from 'probot';
-import { RepoContext } from '../context/repoContext';
+import { Application } from 'probot';
 import { createHandlerPullRequestChange } from './utils';
 import { autoAssignPRToCreator } from './actions/autoAssignPRToCreator';
 import { editOpenedPR } from './actions/editOpenedPR';
 import { updateReviewStatus } from './actions/updateReviewStatus';
-import { autoMergeIfPossible } from './actions/autoMergeIfPossible';
-
-const autoApproveAndAutoMerge = async (
-  context: Context<Webhooks.WebhookPayloadPullRequest>,
-  repoContext: RepoContext,
-): Promise<void> => {
-  // const autoMergeLabel = repoContext.labels['merge/automerge'];
-  const codeApprovedLabel = repoContext.labels['code/approved'];
-  const prLabels = context.payload.pull_request.labels;
-  if (prLabels.find((l): boolean => l.id === codeApprovedLabel.id)) {
-    await context.github.pulls.createReview(
-      context.issue({ event: 'APPROVE' }),
-    );
-  }
-
-  await autoMergeIfPossible(context, repoContext);
-};
+import { autoApproveAndAutoMerge } from './actions/autoApproveAndAutoMerge';
 
 export default function opened(app: Application): void {
   app.on(
