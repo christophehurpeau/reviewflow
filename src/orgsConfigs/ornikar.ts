ef73ec12508e9270ac8511b6a5a5b68616d61650
Christophe Hurpeau <christophe@hurpeau.com>
2019-10-24 08:59:08 +0200

    feat: add alexis to team design

--- src/orgsConfigs/ornikar.ts
+++ src/orgsConfigs/ornikar.ts
@@ -82,6 +82,7 @@ const config: Config<'dev' | 'design', 'archis' | 'frontends' | 'backends'> = {
       jperriere: `julien${process.env.ORNIKAR_EMAIL_DOMAIN}`,
       CoralineColasse: `coraline${process.env.ORNIKAR_EMAIL_DOMAIN}`,
       Lenamari: `lena${process.env.ORNIKAR_EMAIL_DOMAIN}`,
+      'AlexisRiols-Ornikar': `alexis.riols${process.env.ORNIKAR_EMAIL_DOMAIN}`,
     },
   },
 
