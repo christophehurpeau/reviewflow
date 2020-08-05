c16e3530aee0af87f12c56f0b0313d46a5ab1b8e
Christophe Hurpeau <christophe@hurpeau.com>
2020-08-05 22:47:02 +0200

    feat: also requires review request in reviewflow

--- src/accountConfigs/reviewflow.ts
+++ src/accountConfigs/reviewflow.ts
@@ -3,6 +3,7 @@ import chrisconfig from './christophehurpeau';
 
 const config: Config<'dev', never> = {
   ...chrisconfig,
+  requiresReviewRequest: true,
   groups: {
     dev: {
       christophehurpeau: 'christophe@hurpeau.com',
