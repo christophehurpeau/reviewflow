2902da6afe289a47dd12439d843d832260ffdfbf
Christophe Hurpeau <christophe@hurpeau.com>
2021-01-11 11:16:58 +0100

    fix: prettier

--- src/accountConfigs/ornikar.ts
+++ src/accountConfigs/ornikar.ts
@@ -103,7 +103,7 @@ const config: Config<'dev' | 'design', 'ops' | 'frontends' | 'backends'> = {
         'darame07',
         'Pixy',
         'Radyum',
-        'camillebaronnet'
+        'camillebaronnet',
       ],
       labels: ['teams/backend'],
     },
