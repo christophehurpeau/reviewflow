2d848088fdd9646034494be27ab316cfe5e3f686
Maxime <3755401+Mxime@users.noreply.github.com>
2021-01-21 20:57:39 +0100

    chore(account-configs): add Carl Bouville as designer (#169)
    * chore(account-configs): add Carl Bouville as designer
    
    * Update src/accountConfigs/ornikar.ts
    
    Co-authored-by: Christophe Hurpeau <christophe.hurpeau+github@gmail.com>

--- src/accountConfigs/ornikar.ts
+++ src/accountConfigs/ornikar.ts
@@ -86,6 +86,7 @@ const config: Config<'dev' | 'design', 'ops' | 'frontends' | 'backends'> = {
       CoralineColasse: `coraline${process.env.ORNIKAR_EMAIL_DOMAIN}`,
       Lenamari: `lena${process.env.ORNIKAR_EMAIL_DOMAIN}`,
       loicleser: null,
+      carlbouville: `carl.bouville{process.env.ORNIKAR_EMAIL_DOMAIN}`,
     },
   },
 
