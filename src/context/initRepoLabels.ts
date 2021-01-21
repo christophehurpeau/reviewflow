5d7fab4f7d57909275058eb30580482b8e004248
Christophe Hurpeau <christophe@hurpeau.com>
2021-01-21 21:06:04 +0100

    fix: label description can be undefined

--- src/context/initRepoLabels.ts
+++ src/context/initRepoLabels.ts
@@ -7,7 +7,7 @@ export interface LabelResponse {
   node_id: string;
   url: string;
   name: string;
-  description: string | null;
+  description?: string | null;
   color: string;
   default: boolean;
 }
