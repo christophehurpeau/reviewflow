c45ad03a48b44d4702b1e4f50f7acc8aeab6e400
Thibaud Nesztler <picobuntu@gmail.com>
2021-05-17 10:45:18 +0200

    Fix email address

--- src/accountConfigs/ornikar.ts
+++ src/accountConfigs/ornikar.ts
@@ -142,7 +142,7 @@ const config: Config<'dev' | 'design', 'ops' | 'frontends' | 'backends'> = {
       machartier: `marie-anne${process.env.ORNIKAR_EMAIL_DOMAIN}`,
       camillebaronnet: `camille.baronnet${process.env.ORNIKAR_EMAIL_DOMAIN}`,
       'olivier-martinez': `olivier.martinez${process.env.ORNIKAR_EMAIL_DOMAIN}`,
-      tnesztler: `tnesztler${process.env.ORNIKAR_EMAIL_DOMAIN}`
+      tnesztler: `thibaud.nesztler${process.env.ORNIKAR_EMAIL_DOMAIN}`
 
       /* front */
       christophehurpeau: `christophe${process.env.ORNIKAR_EMAIL_DOMAIN}`,
