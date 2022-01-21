e66638837c6ccc5f6bee5cd08b9b452f47e97f2f
Christophe Hurpeau <christophe@hurpeau.com>
2022-01-21 10:40:26 +0000

    fix: lint-pr status condition

--- src/events/pr-handlers/actions/editOpenedPR.ts
+++ src/events/pr-handlers/actions/editOpenedPR.ts
@@ -107,7 +107,7 @@ export const editOpenedPR = async <Name extends EventsWithRepository>(
     },
   };
 
-  if (!hasLegacyLintPrCheck && !previousSha) {
+  if (!hasLegacyLintPrCheck || previousSha) {
     statuses.push(lintStatus);
   }
 
