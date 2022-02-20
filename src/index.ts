15495f1ba4552a3acc284d9b97b435b53e77a9d7
Christophe Hurpeau <christophe@hurpeau.com>
2022-02-20 17:56:49 +0100

    fix: make sure db and server are closed properly

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type { MongoConnection } from 'liwi-mongo';
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import 'dotenv/config';
 import { run } from 'probot';
@@ -20,11 +21,27 @@ console.log({ name: process.env.REVIEWFLOW_NAME });
 
 // let config = await getConfig(context, 'reviewflow.yml');
 
-run((app, { getRouter }) => {
+let mongoConnection: MongoConnection | null = null;
+
+const serverPromise = run((app, { getRouter }) => {
   const mongoStores = mongoInit();
+  mongoConnection = mongoStores.connection;
   const slackHome = createSlackHomeWorker(mongoStores);
   const appContext: AppContext = { mongoStores, slackHome };
   appRouter(app, getRouter, appContext);
   initApp(app, appContext);
   slackHome.scheduleUpdateAllOrgs((id: number) => app.auth(id));
 });
+
+const gracefulExit = function () {
+  Promise.all([
+    serverPromise.then((server) => server.stop()),
+    mongoConnection && mongoConnection.close(),
+  ]).then(() => {
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(0);
+  });
+};
+
+process.on('SIGINT', gracefulExit);
+process.on('SIGTERM', gracefulExit);
