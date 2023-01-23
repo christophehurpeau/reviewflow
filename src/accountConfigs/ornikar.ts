415e541b6ab4d862dd6316513d556250192f81cc
Christophe Hurpeau <christophe@hurpeau.com>
2023-01-23 15:06:20 +0100

    feat: ornikar config allow SCOPED-STAGING to fail

--- src/accountConfigs/ornikar.ts
+++ src/accountConfigs/ornikar.ts
@@ -56,6 +56,7 @@ const config: Config<'dev' | 'design', 'ops' | 'frontends' | 'backends'> = {
     'ci/circleci: test-e2e',
     'ci/circleci: tests-e2e',
     'SonarCloud Code Analysis',
+    'SCOPED-STAGING',
   ],
   disableBypassMergeFor: /(shared-|orb|-configs)/,
   prDefaultOptions: {
