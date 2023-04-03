41b9b8e15914437947f81a56b6cfed3050ea9fe6
Christophe Hurpeau <christophe@hurpeau.com>
2023-04-03 23:51:35 +0200

    fix: skipCheckMergeableState in autoMergeChanged

--- src/events/pr-handlers/autoMergeChanged.ts
+++ src/events/pr-handlers/autoMergeChanged.ts
@@ -50,6 +50,7 @@ export default function autoMergeChangedHandler(
             repoContext,
             reviewflowPrContext,
             context.payload.sender,
+            true,
           ),
       ]);
     },
