d4968874ba9ea2440f8d7b0e6549cefcfa475760
Christophe Hurpeau <christophe@hurpeau.com>
2023-11-01 15:52:16 +0100

    chore: remove warning

--- scripts/check-package.js
+++ scripts/check-package.js
@@ -3,13 +3,6 @@ import { createCheckPackage } from 'check-package-dependencies';
 await createCheckPackage()
   .checkRecommended({
     isLibrary: false,
-    onlyWarnsForInDependencies: {
-      '@commitlint/config-conventional': {
-        duplicateDirectDependency: [
-          'conventional-changelog-conventionalcommits',
-        ],
-      },
-    },
   })
   .checkSatisfiesVersionsFromDependency('probot', {
     dependencies: ['@octokit/core', '@octokit/webhooks'],
