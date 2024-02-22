936329ff5514531bca5b15ae3482fed3e0ced664
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-02-22 18:07:29 +0100

    feat: enable checks/passed for elax

--- src/accountConfigs/Elax-Energie.ts
+++ src/accountConfigs/Elax-Energie.ts
@@ -22,6 +22,10 @@ const config: Config<never> = {
         name: ':green_heart: checks/fail',
         color: githubPalette.dangerEmphasis,
       },
+      'checks/passed': {
+        name: ':green_heart: checks/passed',
+        color: githubPalette.successEmphasis,
+      },
 
       /* infos */
       'breaking-changes': {
