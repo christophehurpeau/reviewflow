8870f94df6462c6fabebe7ec9c182ddf4c613163
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-04-24 22:20:09 +0200

    feat: clean revert of revert

--- src/events/pr-handlers/actions/utils/prTitle.ts
+++ src/events/pr-handlers/actions/utils/prTitle.ts
@@ -18,5 +18,6 @@ export const cleanTitle = (
     .replace(/^([A-Za-z]+)[/:;]\s*/, (s, arg1) => `${arg1.toLowerCase()}: `)
     .replace(/\s+(-\s*)?[()[\]]\s*no[\s-]*isss?ue\s*[()[\]]$/i, ' [no issue]')
     .replace(/^Revert "([^"]+)"( \[no issue])?$/, 'revert: $1$2')
-    .replace(/^(revert:.*)(\s+\(#\d+\))( \[no issue])?$/, '$1$3');
+    .replace(/^(revert:.*)(\s+\(#\d+\))( \[no issue])?$/, '$1$3')
+    .replace(/^(?:revert: ){2}/, '');
 };
