b6e24c7eda2294780d6068a76219b29328cd0556
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-08-13 19:01:54 +0200

    style: format include array in vite.config.ts for better readability

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ export default defineConfig({
   test: {
     testTimeout: 30000,
     setupFiles: ["packages/reviewflow/src/tests/setup.ts"],
-    include: ["packages/reviewflow/src/**/__tests__/**/*.ts?(x)", "packages/reviewflow/src/**/*.test.ts?(x)"],
+    include: [
+      "packages/reviewflow/src/**/__tests__/**/*.ts?(x)",
+      "packages/reviewflow/src/**/*.test.ts?(x)",
+    ],
     coverage: {
       include: ["src/**/*.ts?(x)"],
       reportsDirectory: "docs/coverage",
